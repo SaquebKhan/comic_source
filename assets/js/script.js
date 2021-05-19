74321bd7e57014808e83f05414ac0bb4461b1d2e
Jamie Kaczor <dignanjk@aol.com>
2021-05-19 11:01:27 -0400

    latest js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,31 @@
-
-// var API_KEY = '10159111235406904'
+var searchInput = document.getElementById("search")
+var API_KEY = '10159111235406904'
 // var userInput = 
 
 
 // var requestUrl = 'https://superheroapi.com/api.php/' + API_KEY + '/search/' + userInput;
 
 
-fetch('https://www.superheroapi.com/api.php/10159111235406904/search/ironman')
-  .then(response => {
-    return response.json();
-  })
-  .then(users => {
-    console.log(users);
-  });
 
-  function myFunction() {
-    var x = document.getElementById("myBtn");
-    x.disabled = true;
-  }
+
+/* function myFunction() {
+   var x = document.getElementById("myBtn");
+   x.disabled = true;
+ }
+ */
+
+function goToSearch() {
+
+  var userSearch = searchInput.value;
+  console.log(userSearch);
+  var url = 'https://www.superheroapi.com/api.php/' + API_KEY + '/search/' + userSearch;
+  console.log(url);
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(heroes => {
+      console.log(heroes);
+    });
+  //   window.location.href= 'https://www.superheroapi.com/' + encodeURI(document.getElementById("tag_search").value)
+}
