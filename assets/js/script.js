012b3c860e98c7e3aaa7b9924dd95589fbc5eeff
Jamie Kaczor <dignanjk@aol.com>
2021-05-19 14:16:44 -0400

    we know a little

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,6 @@ var API_KEY = '10159111235406904'
 
 
 
-
-/* function myFunction() {
-   var x = document.getElementById("myBtn");
-   x.disabled = true;
- }
- */
-
 function goToSearch() {
 
   var userSearch = searchInput.value;
@@ -25,7 +18,20 @@ function goToSearch() {
       return response.json();
     })
     .then(heroes => {
+      var heros = heroes.results[0]
       console.log(heroes);
+      $('#hero-name').append(heros.name);
+      $('#hero-combat').append("Combat: " + heros.powerstats.combat);
+      $('#hero-durability').append("Durability: " + heros.powerstats.durability);
+      $('#hero-intelligence').append("Intelligence: " + heros.powerstats.intelligence);
+      $('#hero-power').append("Power: " + heros.powerstats.power);
+      $('#hero-speed').append("Speed: " + heros.powerstats.speed);
+      $('#hero-strength').append("Strength: " + heros.powerstats.strength);
+      
+      
+      console.log(heros.image.url);
+      $('#hero-img').attr("src", heros.image.url)
+      
     });
-  //   window.location.href= 'https://www.superheroapi.com/' + encodeURI(document.getElementById("tag_search").value)
+  
 }
